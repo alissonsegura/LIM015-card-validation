f3b2b21ab5e1c5d9212ec598007d010c3e90316e
alissonsegura <81723288+alissonsegura@users.noreply.github.com>
2021-05-31 15:13:41 -0500

    add login and styless

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import validator from './validator.js';
+//import validator from './validator.js';
+//console.log(validator);
 
-console.log(validator);
+//Hola+Username value
+const username = document.getElementById("username");
+const button = document.getElementById("button");
+button.addEventListener("click" , function () {
+    document.getElementById("login") .style.display ="none";
+});

09e6a4c313a66ff055078677f31913a5be69f5f1
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-06-01 23:33:23 -0500

    add second slide plus cards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 //import validator from './validator.js';
 //console.log(validator);
 
-//Hola+Username value
-const username = document.getElementById("username");
 const button = document.getElementById("button");
-button.addEventListener("click" , function () {
-    document.getElementById("login") .style.display ="none";
+button.addEventListener("click", function () {
+    const username = document.getElementById("name").value
+    document.getElementById("login").style.display = "none";
+    document.getElementById("secondSlide").style.display = "flex"
+    document.getElementById("secondSlide");
+    const h1 = document.getElementById("h1");
+    h1.innerHTML = "Welcome to Iwall-e" + " " + username + "!";
+
 });
+
